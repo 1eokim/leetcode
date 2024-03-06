e4e0a4e21585aa6cb9b95f1b3de5d2aad073ea4d
Leo Kim <leobkkim@gmail.com>
2024-03-07 02:43:45 +0900

    Time: 41 ms (98.15%), Space: 49.6 MB (26.94%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -6,21 +6,14 @@
 var isSubsequence = function(s, t) {
     let subIndex = 0;
     let mainIndex = 0;
-    let matchCount = 0;
 
     while (subIndex < s.length && mainIndex < t.length) {
         if (s[subIndex] === t[mainIndex]) {
             subIndex++;
-            mainIndex++;
-            matchCount++;
-        } else {
-            mainIndex++;
         }
+
+        mainIndex++;
     }
 
-    if (matchCount === s.length) {
-        return true;
-    } else {
-        return false;
-    }
+    return subIndex === s.length;
 };
