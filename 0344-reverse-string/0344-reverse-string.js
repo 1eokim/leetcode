add6ca934c752a13ab9eadfe88ea9134564de4f2
Leo Kim <leobkkim@gmail.com>
2024-03-07 02:59:41 +0900

    Time: 68 ms (92.71%), Space: 57.2 MB (31.37%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,7 +2,7 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
+var reverseString = function (s) {
     let left = 0;
     let right = s.length - 1;
 
