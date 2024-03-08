5d88ed1ae9e0f0bd6d33223336d209fbb809569d
Leo Kim <leobkkim@gmail.com>
2024-03-09 03:36:55 +0900

    Time: 101 ms (11.74%), Space: 56.2 MB (55.1%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,49 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let boundaryIndex = -1;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= 0) {
-            boundaryIndex = i;
-            break;
-        }
-    }
-
     let result = [];
 
-    if (boundaryIndex !== -1) {
-        const positiveNums = nums.slice(boundaryIndex);
-        const negativeNums = nums.slice(0, boundaryIndex);
+    let i = nums.length - 1;
+    let left = 0;
+    let right = nums.length - 1;
 
-        console.log(positiveNums);
-        console.log(negativeNums);
-
-        let positiveNumsPointer = 0;
-        let negativeNumsPointer = negativeNums.length - 1;
-
-        while (negativeNumsPointer >= 0 && positiveNumsPointer < positiveNums.length) {
-            if (positiveNums[positiveNumsPointer] >= Math.abs(negativeNums[negativeNumsPointer])) {
-                result.push(negativeNums[negativeNumsPointer] ** 2);
-                negativeNumsPointer--;
-            } else {
-                result.push(positiveNums[positiveNumsPointer] ** 2);
-                positiveNumsPointer++;
-            }
-        }
-
-        while (positiveNumsPointer < positiveNums.length) {
-            result.push(positiveNums[positiveNumsPointer] ** 2);
-            positiveNumsPointer++;
-        }
-
-        while (negativeNumsPointer >= 0) {
-            result.push(negativeNums[negativeNumsPointer] ** 2);
-            negativeNumsPointer--;
-        }
-    } else {
-        for (let i = nums.length - 1; i >= 0; i--) {
-            result.push(nums[i] ** 2);
+    while (left <= right) {
+        if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+            result[i] = nums[left] ** 2;
+            left++;
+            i--;
+        } else {
+            result[i] = nums[right] ** 2;
+            right--;
+            i--;
         }
     }
 
